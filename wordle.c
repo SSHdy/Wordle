aa9fea2077918e64ca6656164558b92840023d10
SSHdy <52146122+SSHdy@users.noreply.github.com>
2022-05-07 16:59:23 -0400

    Update wordle.c

--- wordle.c
+++ wordle.c
@@ -1,17 +1,3 @@
-///////////////////////////////////////////////////////////////////////////// 
-// INTEGRITY STATEMENT (v3)
-//
-// By signing your name and ID below you are stating that you have agreed
-// to the online academic integrity statement:a
-// https://student.cs.uwaterloo.ca/~cs136/current/assignments/integrity.shtml
-/////////////////////////////////////////////////////////////////////////////
-// I received help from and/or collaborated with: 
- 
-// nobody 
-//  
-// Name: Stan Zhang 
-// login ID: y3676zha 
-///////////////////////////////////////////////////////////////////////////// 
 
 #include "wordle.h"
 #include <string.h>
